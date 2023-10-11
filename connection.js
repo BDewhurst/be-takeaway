ef412c788b3a82fa64613475dc78417103ba39b1
Brad <bradleydewhurst123@outlook.com>
2023-10-11 17:42:13 +0100

    production

--- connection.js
+++ connection.js
@@ -3,10 +3,18 @@ const ENV = process.env.NODE_ENV || 'development';
 
 const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+const config = {};
 
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 require('dotenv').config({
   path: pathToCorrectEnvFile,
 });
 
-module.exports = new Pool();
+module.exports = new Pool(config);
